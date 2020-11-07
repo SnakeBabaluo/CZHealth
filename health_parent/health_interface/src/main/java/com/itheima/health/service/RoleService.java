a725f87708002d62e74eed6d3757f5a217addf7c
bao <>
2020-11-07 15:34:55 +0800

    新增role.mtml页面，角色动态维护相关代码

--- health_parent/health_interface/src/main/java/com/itheima/health/service/RoleService.java
+++ health_parent/health_interface/src/main/java/com/itheima/health/service/RoleService.java
@@ -1,5 +1,8 @@
 package com.itheima.health.service;
 
+import com.itheima.health.entity.PageResult;
+import com.itheima.health.entity.QueryPageBean;
+import com.itheima.health.exception.HealthException;
 import com.itheima.health.pojo.Role;
 
 import java.util.Set;
@@ -16,4 +19,34 @@ public interface RoleService {
      * @return
      */
     Set<Role> findAll();
+
+    /**
+     * 分页查询角色列表
+     * @param queryPageBean
+     * @return
+     */
+    PageResult<Role> findPage(QueryPageBean queryPageBean);
+
+    /**
+     * 添加角色信息
+     * @param role
+     * @param permissionIds
+     * @param menuIds
+     */
+    void add(Role role, Integer[] permissionIds, Integer[] menuIds)throws HealthException;
+
+    /**
+     * 根据ID删除角色信息
+     * @param id
+     */
+    void deleteById(Integer id) throws HealthException;
+
+    /**
+     * 根据ID查询用户信息
+     * @param id
+     * @return
+     */
+    Role findById(Integer id);
+
+    void update(Role role, Integer[] permissionIds, Integer[] menuIds) throws HealthException;
 }
